d34e78224ce74f8ddc6d436bfb9180f30cbf6f76
rortizse-start <rortizse@gmail.com>
2025-09-16 22:23:12 -0600

    Primer commit

---
+++ src/main/java/HolaMundo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rortiz
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        areaRectangulo(2, 5);
+    }
+    public static void areaRectangulo(int a,int b){
+        System.out.println("area:"+(a*b));
+    }
+}
